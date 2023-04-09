5425ed9177f1f00c6cb54329dc606b603f4d8ca9
bryxli <bryanzero765@gmail.com>
2023-04-09 15:46:47 -0500

    about

---
+++ src/about.ts
@@ -0,0 +1,15 @@
+import { SlashCommand, SlashCreator } from 'slash-create';
+
+export default class AboutCommand extends SlashCommand {
+  constructor(creator: SlashCreator) {
+    super(creator, {
+      name: 'about',
+      description: 'display bot information',
+      guildIDs: [process.env.DEVELOPMENT_GUILD_ID]
+    });
+  }
+
+  async run() {
+    return 'Run this command every 30 days to ensure that your Discord account maintains eligibility.';
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import dotenv from 'dotenv';
 import { AnyRequestData, SlashCreator, GatewayServer } from 'slash-create';
-import path from 'path';
-import CatLoggr from 'cat-loggr/ts';
 import Eris from 'eris';
+import AboutCommand from './about';
 
-let dotenvPath = path.join(process.cwd(), '.env');
-if (path.parse(process.cwd()).name === 'dist') dotenvPath = path.join(process.cwd(), '..', '.env');
+dotenv.config({ path: '.env' });
 
-dotenv.config({ path: dotenvPath });
-
-const logger = new CatLoggr().setLevel(process.env.COMMANDS_DEBUG === 'true' ? 'debug' : 'info');
 const client = new Eris.Client(process.env.DISCORD_BOT_TOKEN);
 const creator = new SlashCreator({
   applicationID: process.env.DISCORD_APP_ID,
@@ -17,20 +12,6 @@ const creator = new SlashCreator({
   token: process.env.DISCORD_BOT_TOKEN,
   client
 });
-
-creator.on('debug', (message) => logger.log(message));
-creator.on('warn', (message) => logger.warn(message));
-creator.on('error', (error) => logger.error(error));
-
-creator.on('synced', () => logger.info('Commands synced!'));
-
-creator.on('commandRun', (command, _, ctx) =>
-  logger.info(`${ctx.user.username}#${ctx.user.discriminator} (${ctx.user.id}) ran command ${command.commandName}`)
-);
-
-creator.on('commandRegister', (command) => logger.info(`Registered command ${command.commandName}`));
-
-creator.on('commandError', (command, error) => logger.error(`Command ${command.commandName}:`, error));
 
 creator
   .withServer(
@@ -40,7 +21,7 @@ creator
       });
     })
   )
-  .registerCommandsIn(path.join(__dirname, 'commands'))
+  .registerCommand(AboutCommand)
   .syncCommands();
 
 client.connect();
